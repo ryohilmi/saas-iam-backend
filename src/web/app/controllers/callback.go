38aadd2b65aa14d582d112a2b4f70a8bfaeb8bf1
Ryo Hilmi <ryodm.74@gmail.com>
2024-06-06 13:37:22 +0700

    feat: remove info

--- src/web/app/controllers/callback.go
+++ src/web/app/controllers/callback.go
@@ -6,9 +6,7 @@ import (
 	"errors"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -59,79 +57,30 @@ func CallbackPostHandler(auth *authenticator.Authenticator, db *sql.DB) gin.Hand
 			log.Print(err)
 		}
 
-		var tenant_id string
-		var organization_id string
-		var org_name string
+		var picture string
 		var user_id string
-		var user_roles []string
 
-		origin := ctx.Request.Header.Get("Origin")
-		host, _ := url.Parse(origin)
-		subdomain := strings.Split(host.Host, ".")
-
-		rows, err := db.Query(`	select t.id as tenant_id, o.id as organization_id, o.name as org_name from tenant t
-								left join application a on a.id  = t.app_id
-								left join organization o on o.id = t.org_id
-								where host=$2 and o.subdomain=$1 limit 1`, subdomain[0], subdomain[1])
-
-		if err != nil {
-			log.Printf("err: %v", err)
-		}
-
-		for rows.Next() {
-			err = rows.Scan(&tenant_id, &organization_id, &org_name)
-			if err != nil {
-				log.Print(err)
-			}
-		}
-
-		row := db.QueryRow(`select id from public.user where idp_id=$1`, claims["sub"])
-		err = row.Scan(&user_id)
+		row := db.QueryRow(`select id, picture from public.user 
+							LEFT JOIN public.user_identity
+							on public.user.id = public.user_identity.user_id
+							where idp_id=$1`,
+			claims["sub"])
+		err = row.Scan(&user_id, &picture)
 		if err != nil {
 			log.Print(err)
 		}
 
 		log.Println("User ID: ", user_id)
 
-		rows, err = db.Query(`	select r.name as role
-								from role r
-								left join user_role ur on r.id = ur.role_id 
-								left join user_organization uo on uo.id = ur.user_org_id 
-								left join "user" u on u.id = uo.user_id 
-								left join organization o on o.id = uo.organization_id 
-								left join tenant t on t.id = r.tenant_id 
-								where u.id = $1
-								and t.id = $2
-								and o.id = $3
-							`, user_id, tenant_id, organization_id)
-		if err != nil {
-			log.Print(err)
-		}
-
-		log.Println("User Roles: ", user_roles)
-
-		var role string
-		for rows.Next() {
-			err = rows.Scan(&role)
-			if err != nil {
-				log.Print(err)
-			}
-
-			user_roles = append(user_roles, role)
-		}
-
 		key = []byte(os.Getenv("JWT_SECRET"))
 		t = jwt.NewWithClaims(jwt.SigningMethodHS256,
 			jwt.MapClaims{
-				"iss":       "iam.sashore.com",
-				"sub":       user_id,
-				"exp":       claims["exp"],
-				"iat":       claims["iat"],
-				"name":      claims["name"],
-				"tenant_id": tenant_id,
-				"org_id":    organization_id,
-				"org_name":  org_name,
-				"roles":     user_roles,
+				"iss":     "iam.sashore.com",
+				"sub":     user_id,
+				"picture": picture,
+				"exp":     claims["exp"],
+				"iat":     claims["iat"],
+				"name":    claims["name"],
 			})
 		s, err = t.SignedString(key)
 		if err != nil {
