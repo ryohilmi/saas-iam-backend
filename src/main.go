97c194fb59cd624772a1cd31b9de561eb2d0b069
Fiqi <fiqi296@gmail.com>
2024-06-08 20:29:17 +0700

    fix: db

--- src/main.go
+++ src/main.go
@@ -24,10 +24,14 @@ func main() {
 		log.Fatalf("Failed to initialize the authenticator: %v", err)
 	}
 
-	connStr := fmt.Sprintf("postgresql://postgres:%s@%s:%s/iam?sslmode=disable", os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+	connStr := fmt.Sprintf("postgresql://postgres:%s@%s:%s/iam?sslmode=disable", os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Failed to open the database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping the database: %v", err)
 	}
 
 	defer db.Close()
