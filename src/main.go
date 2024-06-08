222f6a0dbb26d3d21c8d158bda298d37ef559f4d
Fiqi <fiqi296@gmail.com>
2024-06-08 20:09:09 +0700

    fix: fatal kalo ga bisa connect db

--- src/main.go
+++ src/main.go
@@ -27,7 +27,7 @@ func main() {
 	connStr := fmt.Sprintf("postgresql://postgres:%s@%s:%s/iam?sslmode=disable", os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Print(err)
+		log.Fatalf("Failed to open the database: %v", err)
 	}
 
 	defer db.Close()
